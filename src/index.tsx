dbb379d0206a3702224e756b4c4b8dddf7520596
Johan <grimmerw968@gmail.com>
2022-11-01 20:21:06 +0200

    fix: remove strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -15,11 +15,9 @@ setTimeout(() => {
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  /*   <React.StrictMode> */ // mobx render two times
   <BrowserRouter>
     <App />
   </BrowserRouter>
-  /*   </React.StrictMode> */
 );
 
 // If you want to start measuring performance in your app, pass a function
